3e8e8d7737a020786e98adcbfb348bdc2c3316d3
Rafik Elady <rafik.elady@signify.com>
2023-12-04 21:34:56 +0100

    day 01 part 2

--- day01/src/main.rs
+++ day01/src/main.rs
@@ -1,40 +1,57 @@
-//function to get first digit from a string
-fn get_first_digit(s: &str) -> u32 {
-    s.chars().skip_while(|p| !p.is_digit(10)).next().unwrap().to_digit(10).unwrap()
+fn get_first_last_number(s: &str) -> u32 {
+    let v: Vec<&str> = s.matches(char::is_numeric).collect();
+
+    format!("{}{}", v[0], v[v.len() - 1])
+        .parse::<u32>()
+        .unwrap()
 }
 
-//function to get last digit from a string
-fn get_last_digit(s: &str) -> u32 {
-    s.chars().rev().skip_while(|p| !p.is_digit(10)).next().unwrap().to_digit(10).unwrap()
-}
+fn get_first_last_spelled_number(s: &str) -> u32 {
+    let s = s
+        .replace("one", "on1e")
+        .replace("two", "tw2o")
+        .replace("three", "thre3e")
+        .replace("four", "fou4r")
+        .replace("five", "fiv5e")
+        .replace("six", "si6x")
+        .replace("seven", "seve7n")
+        .replace("eight", "eigh8t")
+        .replace("nine", "nin9e");
 
-// function to get the concatenation of the first and last digit
-fn get_concatenation(s: &str) -> u32 {
-    let first_digit = get_first_digit(s);
-    let last_digit = get_last_digit(s);
-    let concatenation = format!("{}{}", first_digit, last_digit);
-    concatenation.parse::<u32>().unwrap()
+    get_first_last_number(&s)
 }
 
 fn main() {
     // read file
-    let input = std::fs::read_to_string("input").unwrap();
-    let sum = input.lines().map(get_concatenation).sum::<u32>();
-    println!("Sum of all numbers: {}", sum);
+    let input = std::fs::read_to_string("inputs/input01").unwrap();
+    let sum = input.lines().map(get_first_last_number).sum::<u32>();
+    println!("Sum of all numbers part 1: {}", sum);
+
+    let sum = input
+        .lines()
+        .map(get_first_last_spelled_number)
+        .sum::<u32>();
+    println!("Sum of all numbers part 2: {}", sum);
 }
 
-// create test
 #[cfg(test)]
 mod tests {
     use super::*;
 
     #[test]
-    fn test_get_first_digit() {
-        assert_eq!(get_first_digit("abc123"), 1);
+    fn test_get_first_last_number() {
+        assert_eq!(get_first_last_number("abc123abbb3"), 13);
+        assert_eq!(get_first_last_number("abc1abbb"), 11);
     }
 
     #[test]
-    fn test_get_last_digit() {
-        assert_eq!(get_last_digit("abc123"), 3);
+    fn test_get_first_last_spelled_number() {
+        assert_eq!(get_first_last_spelled_number("two1nine"), 29);
+        assert_eq!(get_first_last_spelled_number("eightwothree"), 83);
+        assert_eq!(get_first_last_spelled_number("abcone2threexyz"), 13);
+        assert_eq!(get_first_last_spelled_number("xtwone3four"), 24);
+        assert_eq!(get_first_last_spelled_number("4nineeightseven2"), 42);
+        assert_eq!(get_first_last_spelled_number("zoneight234"), 14);
+        assert_eq!(get_first_last_spelled_number("7pqrstsixteen"), 76);
     }
 }
