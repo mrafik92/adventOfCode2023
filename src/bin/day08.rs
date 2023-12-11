c788461b67b8c447bc27047d0a42d2cf2fbcee7f
Rafik Elady <rafik.elady@signify.com>
2023-12-11 22:23:34 +0100

    day 08

--- src/bin/day08.rs
+++ src/bin/day08.rs
@@ -1,9 +1,18 @@
 use std::collections::HashMap;
+use num_integer::lcm;
 use regex::Regex;
+
+extern crate num_integer;
 
 fn main() {
     // read file
     let input = std::fs::read_to_string("inputs/input08").unwrap();
+    let (directions, initial_map) = get_directions_and_map(&input);
+    println!("part1: {}", part_1(initial_map.clone(), directions.clone()));
+    println!("part2: {}", part_2_lcm(initial_map, directions));
+}
+
+fn get_directions_and_map(input: &str) -> (Vec<i32>, HashMap<&str, Vec<&str>>) {
     let directions = input.lines().next().unwrap().chars().map(|x| match x {
         'R' => 1,
         'L' => 0,
@@ -30,27 +39,30 @@ fn main() {
         acc
     });
 
-    let mut current_location = initial_map.get(&"AAA");
-    let mut counter : u128 = 0;
+    (directions, initial_map)
+}
+
+fn part_1(initial_map: HashMap<&str, Vec<&str>>, directions: Vec<i32>) -> u128 {
+    let mut counter: u128 = 0;
     let mut loc = &"AAA";
-    while loc != &"ZZZ"    {
+    while loc != &"ZZZ" {
         let direction = directions[(counter % directions.len() as u128) as usize];
         counter += 1;
-        loc = current_location.unwrap().get(direction as usize).unwrap();
-        current_location = initial_map.get(loc);
+        loc = initial_map.get(loc).unwrap().get(direction as usize).unwrap();
     }
+    counter
+}
 
-    println!("part1: {counter}");
-
-    let mut start_entries : Vec<&str> = vec![];
+#[allow(dead_code)]
+fn part_2_brute_force(initial_map: HashMap<&str, Vec<&str>>, directions: Vec<i32>) -> u128 {
+    let mut start_entries: Vec<&str> = vec![];
     for entry in &initial_map {
         if entry.0.ends_with('A') {
             start_entries.push(entry.0);
         }
     }
-    println!("entries: {:?}", start_entries);
 
-    counter = 0;
+    let mut counter: u128 = 0;
     while !start_entries.iter().all(|loc| loc.ends_with('Z')) {
         let direction = directions[(counter % directions.len() as u128) as usize];
         counter += 1;
@@ -63,8 +75,37 @@ fn main() {
         }
     }
 
+    counter
+}
 
-    println!("part2: {counter}");
+fn part_2_lcm(initial_map: HashMap<&str, Vec<&str>>, directions: Vec<i32>) -> u128 {
+    let mut start_entries: Vec<&str> = vec![];
+    for entry in &initial_map {
+        if entry.0.ends_with('A') {
+            start_entries.push(entry.0);
+        }
+    }
+
+    let mut counter: u128 = 0;
+    let mut locations_counter = vec![];
+    for entry in start_entries {
+        let mut loc = entry;
+        while !loc.ends_with('Z') {
+            let direction = directions[(counter % directions.len() as u128) as usize];
+            counter += 1;
+            loc = initial_map.get(loc).unwrap().get(direction as usize).unwrap();
+        }
+        locations_counter.push(counter);
+        counter = 0;
+    }
+
+    counter = lcm(locations_counter[0], locations_counter[1]);
+
+    for i in locations_counter.iter().skip(2) {
+        counter = lcm(counter, *i);
+    }
+
+    counter
 }
 
 #[cfg(test)]
